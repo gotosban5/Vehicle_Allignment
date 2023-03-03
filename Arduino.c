1d9f84d9d6ddb5ff85c5b8b54f681f9e844dc0c2
gotosban5 <126890042+gotosban5@users.noreply.github.com>
2023-03-03 10:04:20 -0500

    Create Arduino.c

---
+++ Arduino.c
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial) {}
+
+  Wire.begin();
+  mpu.initialize();
+
+  if (mpu.testConnection()) {
+    Serial.println("MPU6050 connection successful");
+  } else {
+    Serial.println("MPU6050 connection failed");
+    while (1) {}
+  }
+
+  mpu.setXAccelOffset(0);
+  mpu.setYAccelOffset(0);
+  mpu.setZAccelOffset(0);
+  mpu.setXGyroOffset(0);
+  mpu.setYGyroOffset(0);
+  mpu.setZGyroOffset(0);
+}
+
+void loop() {
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  float accelX = ax / 16384.0;
+  float accelY = ay / 16384.0;
+  float accelZ = az / 16384.0;
+
+  float gyroX = gx / 131.0;
+  float gyroY = gy / 131.0;
+  float gyroZ = gz / 131.0;
+
+  float dt = 0.01; // time interval in seconds
+
+  float gyroXrate = gyroX;
+  float gyroYrate = gyroY;
+  float gyroZrate = gyroZ;
+
+  float roll = 0.0;
+  float pitch = 0.0;
+  float yaw = 0.0;
+
+  roll += gyroXrate * dt;
+  pitch += gyroYrate * dt;
+  yaw += gyroZrate * dt;
+
+  float rollAccel = atan2(accelY, accelZ) * 180.0 / PI;
+  float pitchAccel = atan2(-accelX, sqrt(accelY * accelY + accelZ * accelZ)) * 180.0 / PI;
+
+  float rollAngle = 0.96 * (roll + gyroXrate * dt) + 0.04 * rollAccel;
+  float pitchAngle = 0.96 * (pitch + gyroYrate * dt) + 0.04 * pitchAccel;
+
+  float ToeAngle = (rollAngle - pitchAngle) / 2;
+
+  Serial.print("Vehicle Angle: ");
+  Serial.println(ToeAngle);
+
+  delay(10);
+}
