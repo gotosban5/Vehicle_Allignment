eb92d91d992228547cb4f17b8390c36cf6ca0ed9
gotosban5 <126890042+gotosban5@users.noreply.github.com>
2023-04-27 11:09:14 -0500

    Update ArduinoDelay.cc

--- ArduinoDelay.cc
+++ ArduinoDelay.cc
@@ -1,49 +1,47 @@
 #include <Wire.h>
-#include <MPU6050.h>
 #include <TimerOne.h>
 #include <SoftwareSerial.h>
+ 
+//Direccion I2C de la IMU
+#define MPU 0x68
+ 
+//Ratios de conversion
+#define A_R 16384.0 // 32768/2
+#define G_R 131.0 // 32768/250
+ 
+//Conversion de radianes a grados 180/PI
+#define RAD_A_DEG = 57.295779
+ 
+//MPU-6050 da los valores en enteros de 16 bits
+//Valores RAW
+volatile int16_t AcX, AcY, AcZ, GyX, GyY, GyZ;
+ 
+//Angulos
+float Acc[2];
+float Gy[3];
+float Angle[3];
 
+String valores;
 
-MPU6050 mpu;
-
+unsigned long tiempo_prev;
 volatile int interruptFlag = 0;
-unsigned long previousTime = 0;
-unsigned long interval = 20; // intervalo en milisegundos
+unsigned long interval = 10; // intervalo en milisegundos
 
 const byte INTERRUPT_PIN = 3;
 volatile unsigned int timerOverflowCount = 0;
-
-// Datos de Calibración
-float accelXzero = 0;
-float accelYzero = 0;
-float accelZzero = 0;
-
-// Filtro complementario Paso Bajo
-float alpha = 0.95; // Constante
-
-// Output angles
-float camber = 0;
-float toe = 0;
-float caster = 0;
-float casterTotal = 0;
-float wheelBase = 2.443; //metros
-float trackWidth = 1.338; //metros
 float dt;
-unsigned long millisOld;
 
 SoftwareSerial analyzer(2, 3);
 
 bool LED_STATE = true;
 
-volatile int16_t accelXraw, accelYraw, accelZraw;
-volatile int16_t gyroXraw, gyroYraw, gyroZraw;
-
 void setup() {
+  Wire.begin(); // remove arguments from begin() method call
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
   Serial.begin(9600);
-  Wire.begin();
-  mpu.initialize();
-  mpu.setFullScaleAccelRange(2);
-  mpu.setFullScaleGyroRange(250); // rango del acelerometro +/- 2g
 
   pinMode(INTERRUPT_PIN, INPUT_PULLUP);
   
@@ -65,8 +63,24 @@ void setup() {
 
 }
 
-void loop(){
-  mpu.getMotion6(&accelXraw, &accelYraw, &accelZraw, &gyroXraw, &gyroYraw, &gyroZraw);
+void loop() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); //Pedir el registro 0x3B - corresponde al AcX
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,6,true);   //A partir del 0x3B, se piden 6 registros
+  AcX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
+  AcY=Wire.read()<<8|Wire.read();
+  AcZ=Wire.read()<<8|Wire.read();
+
+  //Leer los valores del Giroscopio
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,6,true);   //A partir del 0x43, se piden 6 registros
+  GyX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
+  GyY=Wire.read()<<8|Wire.read();
+  GyZ=Wire.read()<<8|Wire.read();
+
 }
 
 ISR(TIMER1_COMPA_vect){
@@ -74,64 +88,34 @@ ISR(TIMER1_COMPA_vect){
   timerOverflowCount++;
   TCNT1  = 0;                  //First, set the timer back to 0 so it resets for next interrupt
   LED_STATE = !LED_STATE;      //Invert LED state
-  digitalWrite(2,LED_STATE);   //Write new state to the LED on pin D5
+  digitalWrite(2,LED_STATE);
 
-  // Conversión de aceleración a m/s^2 y velocidad angular en grad/s
-  float accelX = (float)accelXraw / 16384.0 * 9.81;
-  float accelY = (float)accelYraw / 16384.0 * 9.81;
-  float accelZ = (float)accelZraw / 16384.0 * 9.81;
-  float gyroX = (float)gyroXraw / 131.0;
-  float gyroY = (float)gyroYraw / 131.0;
-  float gyroZ = (float)gyroZraw / 131.0;
+  //A partir de los valores del acelerometro, se calculan los angulos Y, X
+  //respectivamente, con la formula de la tangente.
+  Acc[1] = atan(-1*(AcX/A_R)/sqrt(pow((AcY/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
+  Acc[0] = atan((AcY/A_R)/sqrt(pow((AcX/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
+  //Acc[2] = atan(-1*(AcZ/A_R)/sqrt(pow((AcX/A_R),2) + pow((AcY/A_R),2)))*RAD_TO_DEG;
 
-  accelX -= accelXzero;
-  accelY -= accelYzero;
-  accelZ -= accelZzero;
+  //Calculo del angulo del Giroscopio
+  Gy[0] = GyX/G_R;
+  Gy[1] = GyY/G_R;
+  Gy[2] = GyZ/G_R;
 
-  float roll = 0.0;
-  float pitch = 0.0;
-  float yaw = 0.0;
+  dt = (millis() - tiempo_prev) / 1000.0;
+  tiempo_prev = millis();
+ 
+  //Aplicar el Filtro Complementario
+  Angle[0] = 0.95 *(Angle[0]+Gy[0]*dt) + 0.05*Acc[0];
+  Angle[1] = 0.95 *(Angle[1]+Gy[1]*dt) + 0.05*Acc[1];
 
-  roll += gyroX * dt;
-  pitch += gyroY * dt;
-  yaw += gyroZ * dt;
-
-  float rollAccel = atan(accelX / sqrt(pow(accelY, 2) + pow(accelZ, 2))) * 180.0 / PI;
-  float pitchAccel = atan(accelZ / sqrt(pow(accelX, 2) + pow(accelY, 2))) * 180.0 / PI;
-  // float pitchAccel = atan(-accelX / accelZ) * 180.0 / PI;
-  float yawAccel = atan(accelY / sqrt(pow(accelX, 2) + pow(accelZ, 2)))* 180.0 / PI;
-
-  // Filtro complementario
-
-  float rollAngle = (0.95 * roll) + (0.05 * rollAccel);
-  float pitchAngle = (0.95 * pitch) + (0.05 * pitchAccel);
-  float yawAngle = (0.95 * yaw) + (0.05 * yawAccel);
-
-  // float rollAngle = (0.95 * rollAccel) + (0.05 * roll);
-  // float pitchAngle = (0.95 * pitchAccel) + (0.05 * pitch);
-  // float yawAngle = (0.95 * yawAccel) + (0.05 * yaw);
-
-  // Camber
-  camber = rollAccel;
-
-  // Toe
-  //toe = atan(pitchAngle / pitchAccel) * 180.0 / PI;
-  toe = ((pitchAngle - rollAngle) / 2);
-  //toe = atan(pitchAngle / pitchAccel);
-  //toe = pitchAngle;  
-    
-  // Caster
-  //caster = pitchAngle;
-  //casterTotal = yawAccel;
-  casterTotal = (yawAngle * wheelBase) / trackWidth;
-  //casterTotal = atan((gyroY * cos(pitchAngle) + gyroZ * sin(yawAngle)) / sqrt(pow(accelX, 2) + pow(accelY, 2)));
-
-  Serial.print("Ca:");
-  Serial.print(camber);
-  Serial.print(", T:");
-  Serial.print(toe);
-  Serial.print(", Cs:");
-  Serial.println(casterTotal);
+  //Integración respecto del tiempo paras calcular el YAW
+  Angle[2] = 0.95*(Angle[2] + Gy[2]*dt) + 0.05*Acc[2];
+  //Angle[2] = Angle[2]+Gy[2]*dt;
+ 
+  //Mostrar los valores por consola
+  valores = "Caster: " +String(Angle[0]) + ", Camber: " + String(Angle[1]) + ", Toe: " + String(Angle[2]);
+  Serial.println(valores);
 
   analyzer.write(0x55);
+
 }
